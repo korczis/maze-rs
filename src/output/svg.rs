4facdaf1bb286ca7b9fb90288ae7cbb644194396
Andrew Brampton <bramp@google.com>
2021-12-28 14:09:31 -0800

    Simplify the SVG rendering code a little.

--- src/output/svg.rs
+++ src/output/svg.rs
@@ -61,94 +61,72 @@ pub fn format<T>(
     );
 
     // Draw all the vertical lines (as one long line between the maze doors)
-    let size_x = wall_size;
-    let size_y = cell_size + 2 * wall_size;
-
     for x in 0..grid.x() {
         let start_x = (x as u32 + 1) * (cell_size + wall_size);
-        let mut start_y = None;
 
-        for y in 0..grid.y() {
-            let cell = &grid[x][y];
-            let current_y = y as u32 * (cell_size + wall_size);
+        let mut y = 0;
+        while y < grid.y() {
+            let right = grid.is_linked_indices(x, y, x + 1, y);
+            if !right {
+                let start_y = y as u32 * (cell_size + wall_size);
 
-            let right = grid.is_linked_indices(cell.x(), cell.y(), cell.x() + 1, cell.y());
-            if right {
-                // The wall would stop here, so draw if needed.
-                if let Some(start_y) = start_y {
-                    debug!(
-                        "right: ({}, {}), start: ({}, {}), size({}, {})",
-                        x, y, start_x, start_y, size_x, size_y
-                    );
-                    document = document.add(
-                        Rectangle::new()
-                            .set("x", start_x)
-                            .set("y", start_y)
-                            .set("width", size_x)
-                            .set("height", current_y - start_y + wall_size),
-                    );
+                // Draw until we get to the gap
+                let mut last_y = grid.y();
+                while y < last_y {
+                    y += 1;
+                    if grid.is_linked_indices(x, y, x + 1, y) {
+                        last_y = y;
+                        break;
+                    }
                 }
-                start_y = None;
-            } else if start_y.is_none() {
-                start_y = Some(current_y);
+
+                let end_y = last_y as u32 * (cell_size + wall_size);
+
+                document = document.add(
+                    Rectangle::new()
+                        .set("x", start_x)
+                        .set("y", start_y)
+                        .set("width", wall_size)
+                        .set("height", end_y - start_y + wall_size),
+                );
             }
-        }
 
-        if let Some(start_y) = start_y {
-            let current_y = grid.y() as u32 * (cell_size + wall_size);
-
-            document = document.add(
-                Rectangle::new()
-                    .set("x", start_x)
-                    .set("y", start_y)
-                    .set("width", size_x)
-                    .set("height", current_y - start_y + wall_size),
-            );
+            y += 1;
         }
     }
 
     // Draw all the horiztonal lines (as one long line between the maze doors)
-    let size_x = cell_size + 2 * wall_size;
-    let size_y = wall_size;
-
     for y in 0..grid.y() {
-        let mut start_x = None;
         let start_y = (y as u32 + 1) * (cell_size + wall_size);
 
-        for x in 0..grid.x() {
-            let cell = &grid[x][y];
-            let current_x = x as u32 * (cell_size + wall_size);
+        let mut x = 0;
+        while x < grid.x() {
+            let bottom = grid.is_linked_indices(x, y, x, y + 1);
+            if !bottom {
+                let start_x = x as u32 * (cell_size + wall_size);
 
-            // Bottom - Horizontal
-            let bottom = grid.is_linked_indices(cell.x(), cell.y(), cell.x(), cell.y() + 1);
-            if bottom {
-                if let Some(start_x) = start_x {
-                    debug!(
-                        "bottom: ({}, {}), start: ({}, {}), size({}, {})",
-                        x, y, start_x, start_y, size_x, size_y
-                    );
-                    document = document.add(
-                        Rectangle::new()
-                            .set("x", start_x)
-                            .set("y", start_y)
-                            .set("width", current_x - start_x + wall_size)
-                            .set("height", size_y),
-                    );
+                // Draw until we get to the gap
+                let mut last_x = grid.x();
+                while x < last_x {
+                    x += 1;
+                    if grid.is_linked_indices(x, y, x, y + 1) {
+                        last_x = x;
+                        break;
+                    }
                 }
-                start_x = None;
-            } else if start_x.is_none() {
-                start_x = Some(current_x);
+
+                let end_x = last_x as u32 * (cell_size + wall_size);
+
+                document = document.add(
+                    Rectangle::new()
+                        .set("x", start_x)
+                        .set("y", start_y)
+                        .set("width", end_x - start_x + wall_size)
+                        .set("height", wall_size),
+                );
             }
-        }
-        if let Some(start_x) = start_x {
-            let current_x = grid.x() as u32 * (cell_size + wall_size);
-            document = document.add(
-                Rectangle::new()
-                    .set("x", start_x)
-                    .set("y", start_y)
-                    .set("width", current_x - start_x + wall_size)
-                    .set("height", size_y),
-            );
+
+            x += 1;
         }
     }
 
